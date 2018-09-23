230a189d53d914b8a7a142dea829c52a57e12dda
StephLin <stephen359595@gmail.com>
2018-09-24 01:34:32 +0800

    Avoid quotation marks

--- settings-init.py
+++ settings-init.py
@@ -7,7 +7,7 @@ with open('./webvirtcloud/settings.py.template', 'r') as iFile:
     for line in iFile:
         if "SECRET_KEY" in line:
             haystack = string.ascii_letters + string.digits + string.punctuation
-            config += "SECRET_KEY = '''" + ''.join([random.SystemRandom().choice(haystack) for _ in range(50)]) + "'''\n"
+            config += "SECRET_KEY = '" + ''.join([random.SystemRandom().choice(haystack) for _ in range(50)]).replace('"', '#').replace("'", '$') + "'\n"
             continue
         config += line
 
